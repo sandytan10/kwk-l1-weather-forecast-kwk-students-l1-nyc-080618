4ba4371d50564e879335e1637f1476f36a29c816
sandytan10 <stan10@stuy.edu>
2018-08-13 01:50:06 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -1,15 +1,18 @@
 # Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output. 
 
-# weather = {
-# 	:city => "New York",
-# 	:description => "Partly Cloudy",
-# 	:temperature_farenheit => 66.3
-# }
+ 
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
+
+weather = {
+ 	:city => "New York",
+ 	:description => "Partly Cloudy",
+ 	:temperature_farenheit => 66.3}
 def location(weather)
-	
+weather == {:city => "New York"}
+  return "New York"
 end
+
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
